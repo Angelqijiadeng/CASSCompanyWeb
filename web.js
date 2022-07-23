74d0053d911e64a4b99117d6b03042b56658a940
Angelqijiadeng <105652997+Angelqijiadeng@users.noreply.github.com>
2022-07-22 22:30:13 -0400

    DRAFT

---
+++ web.js
@@ -0,0 +1,20 @@
+const myButton = document.getElementById("top");
+window.onscroll = function (){scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        myButton.style.display = "block";
+    } else{
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+document.getElementById("top").onclick = function (){
+    scrollFunction();
+    topFunction();
+}
